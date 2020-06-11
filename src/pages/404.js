9ea260394ec340df429c009cc3adf6c285b15b7d
권윤하 <jenga@gwon-yunhaui-MacBook-Pro.local>
2020-06-11 11:24:14 +0900

    custom 404 페이지 생성

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,16 @@
+import HolyIcon from "@Img/holy_icon.jpg";
+import "./custom404.scss";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className="custom_404_wrapper">
+      <img alt="404 숫자를 들고 있는 홀리 이모티콘" src={HolyIcon} />
+      <h1>
+        죄송합니다.
+        <br />
+        원하시는 페이지를 찾을 수 없습니다.
+      </h1>
+      <h3>WE CANNOT FIND YOUR DESTINATION.</h3>
+    </div>
+  );
 }

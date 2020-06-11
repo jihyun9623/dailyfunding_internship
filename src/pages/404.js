20ed816338722c3df04e0565d30e807dce9e2248
권윤하 <jenga@gwon-yunhaui-MacBook-Pro.local>
2020-06-11 13:48:43 +0900

    모바일 환경에서 제목이 길어졌을 때 말줄임표 처리되는 문제 해결

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,22 @@
+import Head from "next/head";
+
 import HolyIcon from "@Img/holy_icon.jpg";
 import "./custom404.scss";
 
 export default function Custom404() {
   return (
     <div className="custom_404_wrapper">
+      <Head>
+        <title>데일리 인사이트 :: 페이지를 찾을 수 없습니다.</title>
+        <meta id="og-type" property="og:type" content="website" />
+        <meta
+          id="og-title"
+          property="og:title"
+          content="데일리 인사이트 :: 페이지를 찾을 수 없습니다."
+        />
+        <meta property="og:description" content="페이지를 찾을 수 없습니다." />
+        <meta property="og:image" content="Img/sns_logo.png" />
+      </Head>
       <img alt="404 숫자를 들고 있는 홀리 이모티콘" src={HolyIcon} />
       <h1>
         죄송합니다.
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,11 @@ export default class DailyDocument extends Document {
     return (
       <html>
         <Head>
+          {/* <title>데일리 인사이트</title>
+          <meta id="og-type" property="og:type" content="website" />
+          <meta id="og-title" property="og:title" content="데일리 인사이트" />
+          <meta property="og:description" content="데일리 인사이트" />
+          <meta property="og:image" content="Img/sns_logo.png" /> */}
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
